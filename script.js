bc7b274a77e1cd5c46c86a2af99aaaa07b4be210
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-27 22:22:01 +0500

    Returning Early Pattern from Functions

--- script.js
+++ script.js
@@ -1053,3 +1053,18 @@ function isLess(a, b){
 }
 console.log(isLess(10 , 15));
 
+// =========================================
+//Returning Early Pattern from Functions
+// =========================================
+
+function abTest(a,b){
+
+    if (a<0 || b<0){
+        return undefined;
+    }
+
+    return Math.round(Math.pow(Math.sqrt(a) + Math.sqrt(b), 2));
+}
+console.log(abTest(2,2));
+console.log(abTest(2,-2));
+
