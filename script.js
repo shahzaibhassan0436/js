63fe92d2dc44f2fae5315f27edf646b15aadab30
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-27 17:09:57 +0500

    Comparison with the Strict Equality Operator

--- script.js
+++ script.js
@@ -610,3 +610,16 @@ function testEqual(val){
 }
 
 console.log(testEqual(10));
+
+// =========================================
+//Comparison with the Strict Equality Operator
+// =========================================
+
+function testEqual(val){
+    if(val===7){
+        return "Equal"
+    }
+    return "Not Equal";
+}
+
+console.log(testEqual(7));
