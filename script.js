74fce620ec20254176f7f76116f290454ff9a95b
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-27 14:24:31 +0500

    Return a value from a Function with Return

--- script.js
+++ script.js
@@ -513,3 +513,18 @@ function myOutfit(){
 console.log(myOutfit());
 console.log(outerWear);
 
+
+// =========================================
+// Return a value from a Function with Return
+// =========================================
+
+function minusSeven(num)
+{
+    return num -7;
+}
+console.log(minusSeven(10));
+
+function timesFive(num){
+    return num*5;
+}
+console.log(timesFive(4));
