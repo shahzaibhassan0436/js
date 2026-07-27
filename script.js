a2882eb7463a369299c750c60c1d049c98cfac48
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-27 16:47:12 +0500

    Stand in Line

--- script.js
+++ script.js
@@ -561,3 +561,18 @@ function processArg(num){
 }
 
 processed = processArg(7);
+
+// =========================================
+//Stand in Line
+// =========================================
+
+function nextInLine (arr, item){
+    arr.push(item);
+    return arr.shift();
+}
+
+var testArr = [1,2,3,4,5];
+
+console.log("Before: " + JSON.stringify(testArr));
+console.log(nextInLine(testArr, 6));
+console.log("After: " + JSON.stringify(testArr));
