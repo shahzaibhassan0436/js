cf9af225f51cbcb3d539b6254bab3c05875b3e58
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-27 16:49:31 +0500

    Boolean Values

--- script.js
+++ script.js
@@ -576,3 +576,13 @@ var testArr = [1,2,3,4,5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+// =========================================
+//Boolean Values
+// =========================================
+ 
+function welcomeToBooleans(){
+    return true;
+}
+
+
