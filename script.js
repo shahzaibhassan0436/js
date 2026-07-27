ece8768be8b731d3d81c7dd840500757ec2dc03a
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-27 17:57:30 +0500

    Changing If Else Statements

--- script.js
+++ script.js
@@ -822,3 +822,36 @@ function orderMyLogical(val){
     
 }
 console.log(orderMyLogical(15));
+
+// =========================================
+//Changing If Else Statements
+// =========================================
+
+function testSize(num){
+   
+    if(num<5){
+        return "Tiny"
+    }
+   
+    else if(num<10){
+        return "Small"
+    }
+   
+    else if(num<15){
+        return "Medium"
+    }
+   
+    else if(num<20){
+        return "Large"
+    }
+   
+    else{
+        return "Huge";
+    }
+
+}
+console.log(testSize(4));
+console.log(testSize(9));
+console.log(testSize(14));
+console.log(testSize(19));
+console.log(testSize(25));
