a04c8ff3cce45f30de35cead905af0c14a9e88ce
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-27 21:51:10 +0500

    Replacing If else chains  Switch Statements

--- script.js
+++ script.js
@@ -986,3 +986,59 @@ function sequentialSizes(val){
 console.log(sequentialSizes(1));
 console.log(sequentialSizes(4));
 console.log(sequentialSizes(8));
+
+// =========================================
+//Replacing If else chains  Switch Statements
+// =========================================
+
+function chainToSwitch(val){
+    var answer = "";
+
+    if(val === "bob"){
+        answer = " Marley";
+    }
+
+    else if(val === 42){
+        answer = " The answer";
+    }
+
+    else if(val === 1){
+        answer = " There is no #1";
+    }
+
+    else if(val === 99){
+        answer = " Missed me by this much";
+    }
+
+    else if(val === 7){
+        answer = " Ate Nine";
+    }
+    return answer;
+}
+console.log(chainToSwitch(1));
+
+function chainToSwitch(val){
+    var answer = "";
+    
+    switch(val){
+      case "bob":
+        answer = "Marley";
+        break; 
+
+      case 42:
+        answer = "The Answer";
+        break;  
+      case 1:
+        answer = "There is no #1";
+        break;  
+
+      case 99:
+        answer = "Missed me by this much!";
+        break;  
+
+       case 7:
+        answer = " Ate Nine"; 
+    }
+    return answer;
+}
+console.log(chainToSwitch(42))
