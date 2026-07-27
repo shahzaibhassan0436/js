f21e697298a9b6ef22da43dabba95044384009ca
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-28 02:53:27 +0500

    Accessing Object Properties with Bracket Notation

--- script.js
+++ script.js
@@ -1137,3 +1137,18 @@ var shirtValue = testObj.shirt;
 
 console.log(hatValue);
 console.log(shirtValue);
+
+// =========================================
+//Accessing Object Properties with Bracket Notation
+// =========================================
+
+var testObj = {
+    "hat":"ballcap",
+    "shirt":"jersey",
+    "shoes":"cleats"
+};
+var hatValue = testObj["hat"]
+var shirtValue = testObj['shirt'];
+
+console.log(hatValue);
+console.log(shirtValue);
