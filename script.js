ca50265b7fd6630feadfcb35f10055c47e2c9253
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-28 02:51:59 +0500

    Accessing Object Properties with Dot Notation

--- script.js
+++ script.js
@@ -1122,3 +1122,18 @@ var ourDog = {
     "tails":"1",
     "friends":[],
 };
+
+// =========================================
+//Accessing Object Properties with Dot Notation
+// =========================================
+
+var testObj = {
+    "hat":"ballcap",
+    "shirt":"jersey",
+    "shoes":"cleats"
+};
+var hatValue = testObj.hat
+var shirtValue = testObj.shirt;
+
+console.log(hatValue);
+console.log(shirtValue);
