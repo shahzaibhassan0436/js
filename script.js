96b04322df4b9c799dcd3ee890a82aa46fd06094
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-29 03:47:12 +0500

    Updating an Array Declared with Const

--- script.js
+++ script.js
@@ -1547,3 +1547,19 @@ function printManyTimes(str){
 }
 printManyTimes("FreeCodeCamp");
 
+// =========================================
+// Updating an Array Declared with Const
+// =========================================
+
+const s = [5,7,2]
+function editInPlace(){
+"use strict";
+
+//s = [2,5,7];
+    s[0] = 2;
+    s[1] = 5;
+    s[2] = 7;
+
+}
+editInPlace();
+console.log(s);
