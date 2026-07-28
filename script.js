b9d7e958014dae9c8d3bb65061927d1d04ae2188
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-29 02:22:35 +0500

    Accessing Nested Arrays

--- script.js
+++ script.js
@@ -1289,3 +1289,21 @@ var myStorage = {
 
 var gloveBoxContents = myStorage.car.inside["glove box"];
 console.log(gloveBoxContents)
+
+// =========================================
+//Accessing Nested Arrays
+// =========================================
+
+var myPlants = [
+    {
+        type: "flowers",
+        list: ["rose","tulip", "dandlion"]
+    },
+
+    {
+        type: "trees",
+        list: ["fir", "pine", "birch"]
+    }
+];
+var secondTree = myPlants[1].list[1];
+console.log(secondTree);
