3eddc8dda99788af3d5355c6556f6e846047d93b
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-29 03:13:22 +0500

    Generate Random Fractions

--- script.js
+++ script.js
@@ -1448,3 +1448,15 @@ function randomFraction(){
     return Math.random();
 }
 console.log(randomFraction());
+
+// =========================================
+// Generate Random Fractions
+// =========================================
+
+ var randomNumberBetween0and19 = Math.floor(Math.random()* 20);
+
+function randomWholeNum(){
+
+    return Math.floor(Math.random() *10);
+}
+console.log(randomWholeNum());
