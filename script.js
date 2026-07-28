dedfdca551809b38b7c611070f60593d7f891878
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-29 04:06:54 +0500

    Write Higher Order Arrow Functions

--- script.js
+++ script.js
@@ -1604,3 +1604,15 @@ const magic = () => new Date();
 
 var myConcat = (arr1, arr2) => arr1.concat(arr2);
 console.log(myConcat([1,2], [3,4,5])); 
+
+// =========================================
+// Write Higher Order Arrow Functions
+// =========================================
+
+const increment = (function(){
+    return function increment(number, value = 1){
+        return number + value;
+    };
+})();
+console.log(increment(5,2));
+console.log(increment(5));
