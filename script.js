11bd2d7cbb3662a829d7ea33adafb95978c52038
Shahzaib Hassan <shahzaibhassan0436@gmail.com>
2026-07-29 04:28:27 +0500

    Use the Rest Operator with Function Parameters

--- script.js
+++ script.js
@@ -1616,3 +1616,13 @@ const increment = (function(){
 })();
 console.log(increment(5,2));
 console.log(increment(5));
+
+// =========================================
+//Use the Rest Operator with Function Parameters
+// =========================================
+ var sum = ( function(){
+    return function sum(...args){
+        return args.reduce((a, b) => a + b, 0);
+    };
+ })();
+ console.log(sum(1,2,3));
